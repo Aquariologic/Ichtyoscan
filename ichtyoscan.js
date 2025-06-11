b2e1ab813d7fae3d492e1350283eddc35dcaf05a
Aquariologic <contact@ID-FAune.fr>
2025-06-11 21:46:38 +0200

    Add files via upload

---
+++ ichtyoscan.js
@@ -0,0 +1,37 @@
+const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSv-4gYSHRIJcOSqh8sBEwvodnbtNmALhmAcQLDCk9TWuP7v1Nrkjp3RXWn-SqrZfpNdOfUr0Yrqh8M/pub?output=csv';
+const maladies = {};
+
+function formatText(text) {
+  return text ? text.replace(/\n/g, '<br>') : "";
+}
+
+Papa.parse(SHEET_URL, {
+  download: true,
+  header: true,
+  complete: function(results) {
+    results.data.forEach(row => {
+      if (row.ID) maladies[row.ID.trim()] = row;
+    });
+  }
+});
+
+function showDescription(containerId, id) {
+  const data = maladies[id];
+  if (!data) return;
+
+  const html = `
+    <div class='maladie'>
+      <h3>${formatText(data["Nom maladie"])}</h3>
+      <div class='section'><strong>Origine(s)&nbsp;:<br></strong>${formatText(data["Origine"])}<br></div>
+      ${data["Diagnostic différentiel"] ? `<div class='section'><strong>Diagnostic différentiel&nbsp;:<br></strong>${formatText(data["Diagnostic différentiel"])}<br></div>` : ""}
+      <div class='section'><strong>Causes possibles&nbsp;:<br></strong>${formatText(data["Causes"])}<br></div>
+      <div class='section'><strong>Traitement envisageable&nbsp;:<br></strong>${formatText(data["Traitement"])}<br></div>
+      <div class='section'><strong>Quand consulter son véto&nbsp;?<br></strong>${formatText(data["Quand consulter"])}<br></div>
+      <div class='section'><strong>Prévention&nbsp;:<br></strong>${formatText(data["Prévention"])}<br></div>
+    </div>
+  `;
+
+  const container = document.getElementById(containerId);
+  container.innerHTML = html;
+  container.style.display = 'block';
+}
