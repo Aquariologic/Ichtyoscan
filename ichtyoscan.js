b2cf20fb01100b880b3b843914b5904d76217c00
Aquariologic <contact@ID-FAune.fr>
2025-06-23 12:37:43 +0200

    Update ichtyoscan.js
    menus déroulants 2.0

--- ichtyoscan.js
+++ ichtyoscan.js
@@ -1,20 +1,3 @@
-const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSv-4gYSHRIJcOSqh8sBEwvodnbtNmALhmAcQLDCk9TWuP7v1Nrkjp3RXWn-SqrZfpNdOfUr0Yrqh8M/pub?output=csv';
-const maladies = {};
-
-function formatText(text) {
-  return text ? text.replace(/\n/g, '<br>') : "";
-}
-
-Papa.parse(SHEET_URL, {
-  download: true,
-  header: true,
-  complete: function(results) {
-    results.data.forEach(row => {
-      if (row.ID) maladies[row.ID.trim()] = row;
-    });
-  }
-});
-
 function showDescription(containerId, id) {
   const data = maladies[id];
   if (!data) return;
@@ -31,7 +14,7 @@ function showDescription(containerId, id) {
   let html = `<div class='maladie'>
     <h3>${formatText(data["Nom maladie"])}</h3>`;
 
-  sections.forEach((section, index) => {
+  sections.forEach((section) => {
     const content = formatText(data[section.key]);
     if (content) {
       html += `
@@ -47,11 +30,6 @@ function showDescription(containerId, id) {
   });
 
   html += `</div>`;
-
-  const container = document.getElementById(containerId);
-  container.innerHTML = html;
-  container.style.display = 'block';
-}
 
   const container = document.getElementById(containerId);
   container.innerHTML = html;
