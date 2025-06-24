cb96ccf2fd014b719f7c21ca9bc83a17ec9de5c8
Aquariologic <contact@ID-FAune.fr>
2025-06-24 10:50:25 +0200

    Update Ichtyodosator.js

--- Ichtyodosator.js
+++ Ichtyodosator.js
@@ -57,7 +57,7 @@ function chargerOriginesFiltrees(typeMaladie) {
         { val: "🐍ext", txt: "🐍 Parasite externe (vers)" },
         { val: "🦟ext", txt: "🦟 Parasite externe (poux-crustacés-isopodes)" },
         { val: "🦠ext", txt: "🦠 Parasite externe (protozoaire)" },
-        { val: "🚚", txt: "🚚 traitements préventifs à l'arrivée" }
+        { val: "🚚", txt: "arrivée" }
       ];
 
   const clean = typeMaladie.replace(/\s/g, "");
